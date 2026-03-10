9149368e1b6de8ffac61245a01ad092925104ba0
Namuun0521 <turboldnamuun@gmail.com>
2026-03-10 15:30:07 +0800

    updated

--- app/api/me/route.ts
+++ app/api/me/route.ts
@@ -2,32 +2,32 @@ import { NextResponse } from "next/server";
 import { auth, currentUser } from "@clerk/nextjs/server";
 import { prisma } from "@/app/lib/prisma";
 
-export async function POST() {
-  const { userId } = await auth();
-  if (!userId)
-    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+// export async function POST() {
+//   const { userId } = await auth();
+//   if (!userId)
+//     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
 
-  const u = await currentUser();
-  const email = u?.emailAddresses?.[0]?.emailAddress;
-  if (!email) return NextResponse.json({ error: "No email" }, { status: 400 });
+//   const u = await currentUser();
+//   const email = u?.emailAddresses?.[0]?.emailAddress;
+//   if (!email) return NextResponse.json({ error: "No email" }, { status: 400 });
 
-  const name =
-    [u?.firstName, u?.lastName].filter(Boolean).join(" ").trim() ||
-    u?.username ||
-    null;
+//   const name =
+//     [u?.firstName, u?.lastName].filter(Boolean).join(" ").trim() ||
+//     u?.username ||
+//     null;
 
-  const dbUser = await prisma.user.upsert({
-    where: { email },
-    update: { name },
-    create: {
-      email,
-      name,
-      role: "USER",
-    },
-  });
+//   const dbUser = await prisma.user.upsert({
+//     where: { email },
+//     update: { name },
+//     create: {
+//       email,
+//       name,
+//       role: "USER",
+//     },
+//   });
 
-  return NextResponse.json({ user: dbUser });
-}
+//   return NextResponse.json({ user: dbUser });
+// }
 export async function GET() {
   const { userId } = await auth();
   if (!userId)
