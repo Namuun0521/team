1372255e3fd1b2826edb8b4d0d8a164825da88f5
Enkvvsh <enkuush@Enkuushs-MacBook-Air.local>
2026-03-13 11:26:50 +0800

    search

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -8,9 +8,13 @@ import BecomeFreelancerButton from "./BecomeFreelancerButton";
 import { MobileSidebar } from "./MobileSidebar";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
+<<<<<<< HEAD
 import { NotificationDialog } from "./NotificationDialog";
 import SearchPage from "../search/page";
 import Link from "next/link";
+=======
+import SearchPage from "../search/page";
+>>>>>>> 410e9df (search)
 
 type HeaderProps = {
   cartCount: number;
@@ -40,10 +44,22 @@ export const Header = ({ cartCount }: HeaderProps) => {
               <span>Freelancer.mn</span>
             </div>
 
+<<<<<<< HEAD
             {/* SEARCH */}
             <SearchPage />
 
             {/* MENU */}
+=======
+            {/* <div className="relative hidden lg:block lg:w-[360px] xl:w-[400px]">
+              <Search className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-gray-400" />
+              <input
+                type="text"
+                placeholder="Хайх..."
+                className="w-full rounded-lg bg-gray-100 py-2.5 pl-10 pr-4 text-sm outline-none focus:ring-2 focus:ring-blue-500"
+              />
+            </div> */}
+            <SearchPage />
+>>>>>>> 410e9df (search)
             <div className="hidden items-center gap-4 lg:flex">
               <Button
                 variant="link"
