20ff6479aa4054ea182acd922d9046209a033ba6
Temka-Tec <temkatekoo198@gmail.com>
2026-03-16 11:49:51 +0800

    cs

--- app/_components/SecondFooter.tsx
+++ app/_components/SecondFooter.tsx
@@ -1,7 +1,8 @@
 export const SecondFooter = () => {
   return (
     <footer className="bg-gray-100 py-4 text-center text-sm text-gray-500">
-      &copy; {new Date().getFullYear()} Temuulen. Бүх эрх хуулиар хамгаалагдсан.
+      &copy; {new Date().getFullYear()} Freelancer. Бүх эрх хуулиар
+      хамгаалагдсан.
     </footer>
   );
 };
