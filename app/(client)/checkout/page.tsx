51a0cdc06e301c6da4bff93f5a659e01cb152ecc
Nyamdorj08 <nyamdorjnika@gmail.com>
2026-03-18 18:51:34 +0800

    fixed

--- app/(client)/checkout/page.tsx
+++ app/(client)/checkout/page.tsx
@@ -49,7 +49,7 @@ export default function CheckoutPage() {
 
   return (
     <div className="flex h-screen items-center justify-center">
-      <p className="text-lg">hog...</p>
+      <p className="text-lg">hoghog...</p>
     </div>
   );
 }
