b0a8a7c4a05b1cf27b40338a0b6ad2e753926fc8
Nyamdorj08 <nyamdorjnika@gmail.com>
2026-03-18 14:37:00 +0800

    add GET to webhook

--- app/api/webhook/route.tsx
+++ app/api/webhook/route.tsx
@@ -2,6 +2,9 @@ import { NextRequest, NextResponse } from "next/server";
 import { stripe } from "@/lib/stripe";
 import { prisma } from "@/lib/prisma";
 import Stripe from "stripe";
+export async function GET() {
+  return NextResponse.json({ ok: true });
+}
 
 export async function POST(req: NextRequest) {
   const body = await req.text();
